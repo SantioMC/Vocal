f3f171652e6e5bc032ecb0db252a358853cfdbf7
SantioMC <business@santio.me>
2023-12-10 13:58:20 -0500

    feat: Make port configurable in vite build

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,6 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default ({ mode }: { mode: string }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+	return defineConfig({
+		plugins: [sveltekit()],
+		server: {
+			host: process.env.VITE_HOST ?? '0.0.0.0',
+			port: parseInt(process.env.VITE_PORT ?? '3000')
+		}
+	});
+};
